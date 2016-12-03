640aa940ec05eafc465f16d6f24a4ecb0f346de7
xDest | Zach <destroyer7712@gmail.com>
2016-12-03 00:32:37 -0500

    For debug purpose

--- postScript.js
+++ postScript.js
@@ -6,7 +6,7 @@ function pageLoad() {
     {
         doc.innerHTML+=" <div id=\"post\">";
         doc.innerHTML+= "<div id=\"posttitle\">Test post " + (i+1) + "</div>"; //Get the name and put it there
-        doc.innerHTML+= "<div id=\"postinfo\">By Zachary Carr, 10/07/16 @11:25 PM</div><hr/>";//Get the authoer and date then place here
+        doc.innerHTML+= "<div id=\"postinfo\">By Zachary Carr, 10/07/16AAAAAAAA @11:25 PM</div><hr/>";//Get the authoer and date then place here
         doc.innerHTML+= "<div id=\"postbody\"> I think meetings are going to be every monday after school for 45 minutes or so. I'll be in during scorp time if anyone wants to ask for anything</div></div><br/><br/>";
     }
 }
