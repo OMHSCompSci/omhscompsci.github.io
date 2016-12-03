731a2c472f2721cc10be67ad39d8dd0eeef43fad
xDest | Zach <destroyer7712@gmail.com>
2016-12-03 00:43:33 -0500

    Removed br

--- postScript.js
+++ postScript.js
@@ -5,12 +5,11 @@ function pageLoad() {
     var i = 0;
     for (i = 0; i < 2; i++)
     {
-        //Necessary because doc.innerHTML+= "<div>" automatically adds the </div> after :(
         var toAdd = "";
         toAdd += " <div id=\"post\">";
         toAdd += "<div id=\"posttitle\">Test post " + (i+1) + "</div>"; //Get the name and put it there
         toAdd += "<div id=\"postinfo\">By Zachary Carr, 10/07/16AAAAAAAA @11:25 PM</div><hr/>";//Get the authoer and date then place here
-        toAdd += "<div id=\"postbody\"> I think meetings are going to be every monday after school for 45 minutes or so. I'll be in during scorp time if anyone wants to ask for anything</div></div><br/>";
+        toAdd += "<div id=\"postbody\"> I think meetings are going to be every monday after school for 45 minutes or so. I'll be in during scorp time if anyone wants to ask for anything</div></div>";
         doc.innerHTML += toAdd;
     }
 }
