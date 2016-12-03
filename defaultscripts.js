43359031187783929d12df466f48786be64a70be
xDest | Zach <destroyer7712@gmail.com>
2016-12-03 16:14:08 -0500

    Added a default script

---
+++ defaultscripts.js
@@ -0,0 +1,6 @@
+//Using jQuery lib for JS
+$(document).ready(function () {
+    //There's nothing we really need to do right now with this.
+    //In case you don't know, this is sort of like the main method in java.
+    //It's called as soon as the entire page is loaded.
+});
