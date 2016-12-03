2d00c4621d2daa97ee7042627573be5dad1baee9
xDest | Zach <destroyer7712@gmail.com>
2016-12-03 00:32:09 -0500

    Forgot to create new post div

--- postScript.js
+++ postScript.js
@@ -4,8 +4,9 @@ function pageLoad() {
     //For every post
     for (var i = 0; i < 2; i++)
     {
+        doc.innerHTML+=" <div id=\"post\">";
         doc.innerHTML+= "<div id=\"posttitle\">Test post " + (i+1) + "</div>"; //Get the name and put it there
         doc.innerHTML+= "<div id=\"postinfo\">By Zachary Carr, 10/07/16 @11:25 PM</div><hr/>";//Get the authoer and date then place here
-        doc.innerHTML+= "<div id=\"postbody\"> I think meetings are going to be every monday after school for 45 minutes or so. I'll be in during scorp time if anyone wants to ask for anything</div><br/><br/>";
+        doc.innerHTML+= "<div id=\"postbody\"> I think meetings are going to be every monday after school for 45 minutes or so. I'll be in during scorp time if anyone wants to ask for anything</div></div><br/><br/>";
     }
 }
