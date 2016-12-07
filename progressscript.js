ba5bd5e82560562f1e137c50b53577c762916add
xDest | Zach <destroyer7712@gmail.com>
2016-12-06 22:34:00 -0500

    Added cool fonts

--- progressscript.js
+++ progressscript.js
@@ -14,7 +14,24 @@ $(document).ready(function() {
         }
         commitsString+="</table>";
         
-        $("#comlist").html(commitsString);      //alert($commitArray[0].commit.message);
+        $("#comlistgame").html(commitsString);      //alert($commitArray[0].commit.message);
+       });
+    $.get("https://api.github.com/repositories/75451699/commits", function(val) {
+        //This actually works
+        $commitArray = val;
+        var maxCommits = 10;
+        var commitsString = "";
+        commitsString+="<table class=\"comtable\">";
+        for (var i = 0; i < maxCommits; i++) {
+            commitsString+=("<tr class=\"commit-item\">");
+            name = checkSubs($commitArray[i].commit.author.name) + ", " + formatDate($commitArray[i].commit.author.date);
+            commitsString+=("<td>" + name + "</td>");
+            commitsString+=("<td>" + $commitArray[i].commit.message + "</td>");
+            commitsString+="</tr>";
+        }
+        commitsString+="</table>";
+        
+        $("#comlistsite").html(commitsString);      //alert($commitArray[0].commit.message);
        });
 });
 
